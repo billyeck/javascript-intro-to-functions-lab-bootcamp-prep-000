969649f35f198dcd5dc53a664dd4b6aaf0bd658f
William Eck <billyeck@gmail.com>
2019-04-16 16:37:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ function whisper(string){
 function logShout(string){
   console.log(string.toUpperCase())
 }
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string){
+  if (string===string.toLowerCase){
+  return "I can't hear you!"}
+  if (string===string.toUpperCase){
+    return "YES INDEED!"
+  }
+}
